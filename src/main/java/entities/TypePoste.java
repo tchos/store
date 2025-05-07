4fa84096ef463f1098971a24b0cbaecefacf04f4
tchos <kwenol@yahoo.fr>
2025-05-07 03:13:45 +0100

    Couche entities

---
+++ src/main/java/entities/TypePoste.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum TypePoste {
+    CADRE,
+    CHEF_BUREAU,
+    CHEF_SERVICE,
+    DIRECTEUR,
+    DIRECTEUR_GENERAL
+}
---
+++ src/main/java/tchos/store/store/StoreApplication.java
@@ -0,0 +1,15 @@
+package tchos.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("entities")
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
